ea395ce837baaaa008ba27d618d23a8450dda249
Artur Bier <izatop@gmail.com>
2019-07-08 23:48:14 +0300

    fix: update deps and bugfixes

--- packages/api/src/Form/List/MapContainer.tsx
+++ packages/api/src/Form/List/MapContainer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {IterableReceiver} from "../../Context";
+import {IterableReceiver, StoreContext} from "../../Context";
 import {ElementIterableHelper, Store} from "../../Store";
 
 export type MapContainerChildren = (store: Store<any>, helper: ElementIterableHelper) => React.ReactNode;
@@ -11,9 +11,9 @@ export interface IMapContainerProps {
 export class MapContainer<P = {}> extends IterableReceiver<P & IMapContainerProps> {
     public render() {
         return this.context.map((store, id) => (
-            <React.Fragment key={id}>
+            <StoreContext.Provider key={id} value={store}>
                 {this.props.children(store, new ElementIterableHelper(this.context, store))}
-            </React.Fragment>
+            </StoreContext.Provider>
         ));
     }
 }
