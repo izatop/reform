0565ae0c52a0120ec6f191d7655f2bf01d4a7f0f
Artur Bier <izatop@gmail.com>
2020-05-05 18:12:17 +0300

    fix: navbar

--- packages/bulma/src/components/Navbar/Navbar.tsx
+++ packages/bulma/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {ReactElement} from "react";
-import {MakeBreakpoint, XProps} from "../../interfaces";
+import {MakeProps, XProps} from "../../interfaces";
 import {IsColor} from "../../props";
 import {ConfigFactory} from "../../utils";
 import {NavbarContext} from "./props";
@@ -10,17 +10,32 @@ const {Provider} = NavbarContext;
 export interface INavbar extends IsColor {
     shadow?: boolean;
     transparent?: boolean;
+    fixed?: boolean | "bottom" | "top";
+    spaced?: boolean;
 }
 
 export interface INavbarProps extends XProps<"nav"> {
     children: ReactElement | [(ReactElement | boolean | null), (ReactElement | boolean | null)?];
 }
 
-const config = ConfigFactory.create({component: "navbar"});
+const config = ConfigFactory.create({
+    component: "navbar",
+    resolvers: {
+        spaced: "is-spaced",
+        fixed: (v) => v === true ? "is-fixed-top" : `is-fixed-${v}`,
+    },
+});
 
-export const Navbar = config.factory<MakeBreakpoint<INavbar>, INavbarProps>(({props, children}) => {
+export const Navbar = config.factory<MakeProps<INavbar>, INavbarProps>(({props, options: {fixed}, children}) => {
     const [state, setState] = React.useState(false);
     const toggle = React.useCallback(() => setState(!state), [state]);
+    if (fixed) {
+        const bodyClass = fixed === true || fixed === "top" ? "has-navbar-fixed-top" : "has-navbar-fixed-bottom";
+        if (!document.body.classList.contains(bodyClass)) {
+            document.body.classList.add(bodyClass)
+        }
+    }
+
     return (
         <nav {...props}
              aria-label="main navigation"
