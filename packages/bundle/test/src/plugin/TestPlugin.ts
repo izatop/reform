311fe7114db82f0fe5b64d61ddf3fbed27272e19
abir <izatop@gmail.com>
2021-08-28 21:34:04 +0300

    fix: test

--- packages/bundle/test/src/plugin/TestPlugin.ts
+++ packages/bundle/test/src/plugin/TestPlugin.ts
@@ -3,7 +3,7 @@ import {PluginAbstract} from "../../../src";
 export class TestPlugin extends PluginAbstract {
     public readonly name = "test-plugin";
 
-    protected configure() {
+    public configure() {
         // test
     }
 }
