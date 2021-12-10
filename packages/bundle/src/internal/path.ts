9d6554a011e15906bea49318fba782dbfe1cb07e
abir <izatop@gmail.com>
2021-12-10 19:08:30 +0300

    fix: a file resolution direction

--- packages/bundle/src/internal/path.ts
+++ packages/bundle/src/internal/path.ts
@@ -11,7 +11,8 @@ import {assert} from "./assert";
 export function resolveThrough(directory: string, resource: string): string | undefined {
     const paths = directory
         .split(path.sep)
-        .map((s, i, a) => path.resolve(path.sep, a.slice(0, i + 1).join("/")));
+        .map((s, i, a) => path.resolve(path.sep, a.slice(0, i + 1).join("/")))
+        .reverse();
 
     for (const next of paths) {
         const filename = path.join(next, resource);
